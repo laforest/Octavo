da2f8410465ca62256feb967edda7b6683bb4557
Charles Eric LaForest <laforest@eecg.utoronto.ca>
2014-03-02 21:45:22 -0500

    Finished first draft of Branch_Folding
    Propagated removal of useless FLAGS write parameters
    'generate' block for consecutive Branch_Check_Mapped memory-maps
    Included recirculation of jump/IO_ready to retry annulled instructions
    TODO: implement annulling branches
    TODO: consider last bit for I/O branch conditions

--- Octavo/Branching/Branch_Folding.v
+++ Octavo/Branching/Branch_Folding.v
@@ -43,8 +43,6 @@ module Branch_Folding
     parameter   CONDITION_RAMSTYLE                  = 0,
     parameter   CONDITION_INIT_FILE                 = 0,
 
-    parameter   FLAGS_WRITE_WORD_OFFSET             = 0,
-    parameter   FLAGS_WRITE_ADDR_OFFSET             = 0,
     parameter   FLAGS_WORD_WIDTH                    = 0,
     parameter   FLAGS_ADDR_WIDTH                    = 0
 )
@@ -109,8 +107,98 @@ module Branch_Folding
         .out    (jump_previous)
     );
 
+// -----------------------------------------------------------
 
+    wire    [(BRANCH_COUNT * PC_WIDTH)-1:0]     branch_destinations;
+    wire    [ BRANCH_COUNT            -1:0]     jumps;
 
+    genvar count;
+
+    generate
+        for(count = 0; count < BRANCH_COUNT; count = count + 1) begin
+            Branch_Check_Mapped
+            #(
+                .PC_WIDTH                       (PC_WIDTH),
+                .D_OPERAND_WIDTH                (D_OPERAND_WIDTH),
+                .WORD_WIDTH                     (WORD_WIDTH),
+
+                .INITIAL_THREAD                 (INITIAL_THREAD),
+                .THREAD_COUNT                   (THREAD_COUNT),
+                .THREAD_ADDR_WIDTH              (THREAD_ADDR_WIDTH),
+
+                .ORIGIN_WRITE_WORD_OFFSET       (ORIGIN_WRITE_WORD_OFFSET),
+                .ORIGIN_WRITE_ADDR_OFFSET       (ORIGIN_WRITE_ADDR_OFFSET +(ORIGIN_DEPTH * count)),
+                .ORIGIN_WORD_WIDTH              (ORIGIN_WORD_WIDTH),
+                .ORIGIN_ADDR_WIDTH              (ORIGIN_ADDR_WIDTH),
+                .ORIGIN_DEPTH                   (ORIGIN_DEPTH),
+                .ORIGIN_RAMSTYLE                (ORIGIN_RAMSTYLE),
+                .ORIGIN_INIT_FILE               (ORIGIN_INIT_FILE),
+
+                .DESTINATION_WRITE_WORD_OFFSET  (DESTINATION_WRITE_WORD_OFFSET),
+                .DESTINATION_WRITE_ADDR_OFFSET  (DESTINATION_WRITE_ADDR_OFFSET + (DESTINATION_DEPTH * count)),
+                .DESTINATION_WORD_WIDTH         (DESTINATION_WORD_WIDTH),
+                .DESTINATION_ADDR_WIDTH         (DESTINATION_ADDR_WIDTH),
+                .DESTINATION_DEPTH              (DESTINATION_DEPTH),
+                .DESTINATION_RAMSTYLE           (DESTINATION_RAMSTYLE),
+                .DESTINATION_INIT_FILE          (DESTINATION_INIT_FILE),
+
+                .CONDITION_WRITE_WORD_OFFSET    (CONDITION_WRITE_WORD_OFFSET),
+                .CONDITION_WRITE_ADDR_OFFSET    (CONDITION_WRITE_ADDR_OFFSET + (CONDITION_DEPTH * count)),
+                .CONDITION_WORD_WIDTH           (CONDITION_WORD_WIDTH),
+                .CONDITION_ADDR_WIDTH           (CONDITION_ADDR_WIDTH),
+                .CONDITION_DEPTH                (CONDITION_DEPTH),
+                .CONDITION_RAMSTYLE             (CONDITION_RAMSTYLE),
+                .CONDITION_INIT_FILE            (CONDITION_INIT_FILE),
+
+                .FLAGS_WORD_WIDTH               (FLAGS_WORD_WIDTH),
+                .FLAGS_ADDR_WIDTH               (FLAGS_ADDR_WIDTH)
+            )
+            BCM
+            (
+                .clock                          (clock),
+                .PC                             (PC),
+                .flags                          (flags),
+                .IO_ready_previous              (IO_ready_previous),
+                .jump_previous                  (jump_previous),
+
+                .ALU_write_addr                 (ALU_write_addr),
+                .ALU_write_data                 (ALU_write_data),
+
+                .branch_destination             (branch_destinations),
+                .jump                           (jumps)
+            );
+        end
+    endgenerate
+
+// -----------------------------------------------------------
+
+    OR_Reducer
+    #(
+        .WORD_WIDTH     (PC_WIDTH),
+        .WORD_COUNT     (BRANCH_COUNT),
+        .REGISTERED     (`TRUE)
+    )
+    BD_reducer
+    (
+        .clock          (clock),
+        .in             (branch_destinations),
+        .out            (branch_destination)
+    );
+
+// -----------------------------------------------------------
+
+    OR_Reducer
+    #(
+        .WORD_WIDTH     (1),
+        .WORD_COUNT     (BRANCH_COUNT),
+        .REGISTERED     (`TRUE)
+    )
+    jump_reducer
+    (
+        .clock          (clock),
+        .in             (jumps),
+        .out            (jump)
+    );
 
 endmodule
 
