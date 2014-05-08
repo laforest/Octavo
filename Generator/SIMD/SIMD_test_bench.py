6164a06b5ab1b445d2e29b4ad8d5d8cbd7199f3d
Charles Eric LaForest <laforest@eecg.utoronto.ca>
2014-05-08 14:40:49 -0400

    SIMD test harness builds. Not testbenched yet.
    This version keeps Empty/Full bits in SIMD lane.
    1 SIMD lane, 1 cycle delay (hardcoded in SIMD.v)
    Fmax: 454/479/492

--- Generator/SIMD/SIMD_test_bench.py
+++ Generator/SIMD/SIMD_test_bench.py
@@ -6,9 +6,12 @@ import sys
 
 from Misc import misc, parameters_misc
 
-default_bench       = "Hailstone/hailstone_numbers"
-SIMD_default_bench  = "Hailstone/${SIMD_MEM_INIT_FILE_PREFIX}hailstone_numbers"
+bench_dir           = "Hailstone_Stencil_Overhead"
+bench_name          = "hailstone_stencil_overhead"
+default_bench       = bench_dir + "/" + bench_name
+SIMD_default_bench  = bench_dir + "/" + "${SIMD_MEM_INIT_FILE_PREFIX}" + bench_name
 install_base        = misc.base_install_path()
+quartus_base_path   = misc.quartus_base_path
 
 def test_bench(parameters, default_bench = default_bench, SIMD_default_bench = SIMD_default_bench, install_base = install_base):
     assembler_base = os.path.join(install_base, "Assembler")
@@ -19,6 +22,8 @@ def test_bench(parameters, default_bench = default_bench, SIMD_default_bench = S
     parameter       B_WORD_WIDTH                = ${B_WORD_WIDTH},
     parameter       SIMD_A_WORD_WIDTH           = ${SIMD_A_WORD_WIDTH},
     parameter       SIMD_B_WORD_WIDTH           = ${SIMD_B_WORD_WIDTH},
+
+    parameter       INSTR_WIDTH                 = ${INSTR_WIDTH},
 
     parameter       A_IO_READ_PORT_COUNT        = ${A_IO_READ_PORT_COUNT},
     parameter       A_IO_WRITE_PORT_COUNT       = ${A_IO_WRITE_PORT_COUNT},
@@ -32,33 +37,59 @@ def test_bench(parameters, default_bench = default_bench, SIMD_default_bench = S
 
     parameter       SIMD_LANE_COUNT             = ${SIMD_LANE_COUNT},
 
-    parameter       A_INIT_FILE                 = "${assembler_base}/${default_bench}.mem",
-    parameter       B_INIT_FILE                 = "${assembler_base}/${default_bench}.mem",
-    parameter       I_INIT_FILE                 = "${assembler_base}/${default_bench}.mem",
-    parameter       PC_INIT_FILE                = "${assembler_base}/${default_bench}.pc",
-    parameter       SIMD_A_INIT_FILE            = "${assembler_base}/${SIMD_default_bench}.mem",
-    parameter       SIMD_B_INIT_FILE            = "${assembler_base}/${SIMD_default_bench}.mem"
+    parameter       A_INIT_FILE                 = "${assembler_base}/${default_bench}.A",
+    parameter       B_INIT_FILE                 = "${assembler_base}/${default_bench}.B",
+    parameter       I_INIT_FILE                 = "${assembler_base}/${default_bench}.I",
+    parameter       PC_INIT_FILE                = "${assembler_base}/${default_bench}.PC",
+    parameter       SIMD_A_INIT_FILE            = "${assembler_base}/${SIMD_default_bench}.A",
+    parameter       SIMD_B_INIT_FILE            = "${assembler_base}/${SIMD_default_bench}.B",
+
+    parameter       A_DEFAULT_OFFSET_INIT_FILE  = "${assembler_base}/${default_bench}.ADO",
+    parameter       B_DEFAULT_OFFSET_INIT_FILE  = "${assembler_base}/${default_bench}.BDO",
+    parameter       D_DEFAULT_OFFSET_INIT_FILE  = "${assembler_base}/${default_bench}.DDO",
+
+    parameter       A_PROGRAMMED_OFFSETS_INIT_FILE  = "${assembler_base}/${default_bench}.APO",
+    parameter       B_PROGRAMMED_OFFSETS_INIT_FILE  = "${assembler_base}/${default_bench}.BPO",
+    parameter       D_PROGRAMMED_OFFSETS_INIT_FILE  = "${assembler_base}/${default_bench}.DPO",
+
+    parameter       A_INCREMENTS_INIT_FILE  = "${assembler_base}/${default_bench}.AIN",
+    parameter       B_INCREMENTS_INIT_FILE  = "${assembler_base}/${default_bench}.BIN",
+    parameter       D_INCREMENTS_INIT_FILE  = "${assembler_base}/${default_bench}.DIN",
+
+    parameter       ORIGIN_INIT_FILE       = "${assembler_base}/${default_bench}.BO",
+    parameter       DESTINATION_INIT_FILE  = "${assembler_base}/${default_bench}.BD",
+    parameter       CONDITION_INIT_FILE    = "${assembler_base}/${default_bench}.BC",
+    parameter       PREDICTION_INIT_FILE    = "${assembler_base}/${default_bench}.BP",
+    parameter       PREDICTION_ENABLE_INIT_FILE    = "${assembler_base}/${default_bench}.BPE"
 )
 (
     output  reg     [(A_WORD_WIDTH * A_IO_READ_PORT_COUNT)-1:0]                                 A_in,
     output  wire    [(               A_IO_READ_PORT_COUNT)-1:0]                                 A_rden,
+    output  reg     [(               A_IO_READ_PORT_COUNT)-1:0]                                 A_in_EF,
     output  wire    [(A_WORD_WIDTH * A_IO_WRITE_PORT_COUNT)-1:0]                                A_out, 
     output  wire    [(               A_IO_WRITE_PORT_COUNT)-1:0]                                A_wren,
+    output  reg     [(               A_IO_WRITE_PORT_COUNT)-1:0]                                A_out_EF,
     
     output  reg     [(B_WORD_WIDTH * B_IO_READ_PORT_COUNT)-1:0]                                 B_in,
     output  wire    [(               B_IO_READ_PORT_COUNT)-1:0]                                 B_rden,
+    output  reg     [(               B_IO_READ_PORT_COUNT)-1:0]                                 B_in_EF,
     output  wire    [(B_WORD_WIDTH * B_IO_WRITE_PORT_COUNT)-1:0]                                B_out, 
     output  wire    [(               B_IO_WRITE_PORT_COUNT)-1:0]                                B_wren,
+    output  reg     [(               B_IO_WRITE_PORT_COUNT)-1:0]                                B_out_EF,
 
     output  reg     [(SIMD_A_WORD_WIDTH * SIMD_A_IO_READ_PORT_COUNT  * SIMD_LANE_COUNT)-1:0]    SIMD_A_in,
     output  wire    [(                    SIMD_A_IO_READ_PORT_COUNT  * SIMD_LANE_COUNT)-1:0]    SIMD_A_rden,
+    output  reg     [(                    SIMD_A_IO_READ_PORT_COUNT  * SIMD_LANE_COUNT)-1:0]    SIMD_A_in_EF,
     output  wire    [(SIMD_A_WORD_WIDTH * SIMD_A_IO_WRITE_PORT_COUNT * SIMD_LANE_COUNT)-1:0]    SIMD_A_out, 
     output  wire    [(                    SIMD_A_IO_WRITE_PORT_COUNT * SIMD_LANE_COUNT)-1:0]    SIMD_A_wren,
+    output  reg     [(                    SIMD_A_IO_READ_PORT_COUNT  * SIMD_LANE_COUNT)-1:0]    SIMD_A_out_EF,
     
     output  reg     [(SIMD_B_WORD_WIDTH * SIMD_B_IO_READ_PORT_COUNT  * SIMD_LANE_COUNT)-1:0]    SIMD_B_in,
     output  wire    [(                    SIMD_B_IO_READ_PORT_COUNT  * SIMD_LANE_COUNT)-1:0]    SIMD_B_rden,
+    output  reg     [(                    SIMD_B_IO_READ_PORT_COUNT  * SIMD_LANE_COUNT)-1:0]    SIMD_B_in_EF,
     output  wire    [(SIMD_B_WORD_WIDTH * SIMD_B_IO_WRITE_PORT_COUNT * SIMD_LANE_COUNT)-1:0]    SIMD_B_out, 
     output  wire    [(                    SIMD_B_IO_WRITE_PORT_COUNT * SIMD_LANE_COUNT)-1:0]    SIMD_B_wren    
+    output  reg     [(                    SIMD_B_IO_READ_PORT_COUNT  * SIMD_LANE_COUNT)-1:0]    SIMD_B_out_EF,
 );
     integer     cycle;
     reg         clock;
@@ -68,11 +99,19 @@ def test_bench(parameters, default_bench = default_bench, SIMD_default_bench = S
         cycle       = 0;
         clock       = 0;
         half_clock  = 0;
-        A_in        = 0;
-        B_in        = 0;
-        SIMD_A_in   = 0;
-        SIMD_B_in   = 0;
-        `DELAY_CLOCK_CYCLES(1000) $$stop;
+        A_in        = -1;
+        B_in        = -1;
+        A_in_EF     = -1;
+        A_out_EF    = 0;
+        B_in_EF     = -1;
+        B_out_EF    = 0;
+        SIMD_A_in        = -1;
+        SIMD_B_in        = -1;
+        SIMD_A_in_EF     = -1;
+        SIMD_A_out_EF    = 0;
+        SIMD_B_in_EF     = -1;
+        SIMD_B_out_EF    = 0;
+        `DELAY_CLOCK_CYCLES(200000) $$stop;
     end
 
     always begin
@@ -87,23 +126,23 @@ def test_bench(parameters, default_bench = default_bench, SIMD_default_bench = S
         cycle = cycle + 1;
     end
 
-    always begin
-        `DELAY_CLOCK_CYCLES(300)
-        A_in        = -1;
-        B_in        = -1;
-        SIMD_A_in   = -1;
-        SIMD_B_in   = -1;
-        `DELAY_CLOCK_CYCLES(300)
-        A_in        = 0;
-        B_in        = 0;
-        SIMD_A_in   = 0;
-        SIMD_B_in   = 0;
-        `DELAY_CLOCK_CYCLES(300)
-        A_in        = -1;
-        B_in        = -1;
-        SIMD_A_in   = -1;
-        SIMD_B_in   = -1;
-    end
+//    always begin
+//        `DELAY_CLOCK_CYCLES(300)
+//        A_in        = -1;
+//        B_in        = -1;
+//        SIMD_A_in   = -1;
+//        SIMD_B_in   = -1;
+//        `DELAY_CLOCK_CYCLES(300)
+//        A_in        = 0;
+//        B_in        = 0;
+//        SIMD_A_in   = 0;
+//        SIMD_B_in   = 0;
+//        `DELAY_CLOCK_CYCLES(300)
+//        A_in        = -1;
+//        B_in        = -1;
+//        SIMD_A_in   = -1;
+//        SIMD_B_in   = -1;
+//    end
 
     localparam SIMD_WREN_OTHER_DEFAULT  = {(SIMD_LANE_COUNT){`HIGH}};
     localparam SIMD_ALU_C_IN_DEFAULT    = {(SIMD_LANE_COUNT){`LOW}};
@@ -117,7 +156,26 @@ def test_bench(parameters, default_bench = default_bench, SIMD_default_bench = S
         .I_INIT_FILE        (I_INIT_FILE),
         .PC_INIT_FILE       (PC_INIT_FILE),
         .SIMD_A_INIT_FILE   (SIMD_A_INIT_FILE),
-        .SIMD_B_INIT_FILE   (SIMD_B_INIT_FILE)
+        .SIMD_B_INIT_FILE   (SIMD_B_INIT_FILE),
+
+        .A_DEFAULT_OFFSET_INIT_FILE     (A_DEFAULT_OFFSET_INIT_FILE),
+        .B_DEFAULT_OFFSET_INIT_FILE     (B_DEFAULT_OFFSET_INIT_FILE),
+        .D_DEFAULT_OFFSET_INIT_FILE     (D_DEFAULT_OFFSET_INIT_FILE),
+
+        .A_PROGRAMMED_OFFSETS_INIT_FILE     (A_PROGRAMMED_OFFSETS_INIT_FILE),
+        .B_PROGRAMMED_OFFSETS_INIT_FILE     (B_PROGRAMMED_OFFSETS_INIT_FILE),
+        .D_PROGRAMMED_OFFSETS_INIT_FILE     (D_PROGRAMMED_OFFSETS_INIT_FILE),
+
+        .A_INCREMENTS_INIT_FILE     (A_INCREMENTS_INIT_FILE),
+        .B_INCREMENTS_INIT_FILE     (B_INCREMENTS_INIT_FILE),
+        .D_INCREMENTS_INIT_FILE     (D_INCREMENTS_INIT_FILE),
+
+        .ORIGIN_INIT_FILE           (ORIGIN_INIT_FILE),
+        .DESTINATION_INIT_FILE      (DESTINATION_INIT_FILE),
+        .CONDITION_INIT_FILE        (CONDITION_INIT_FILE),
+        .PREDICTION_INIT_FILE        (PREDICTION_INIT_FILE),
+        .PREDICTION_ENABLE_INIT_FILE        (PREDICTION_ENABLE_INIT_FILE)
+
     )
     DUT 
     (
@@ -137,23 +195,31 @@ def test_bench(parameters, default_bench = default_bench, SIMD_default_bench = S
 
         .A_in               (A_in),
         .A_rden             (A_rden),
+        .A_in_EF            (A_in_EF),
         .A_out              (A_out),
         .A_wren             (A_wren),
-        
+        .A_out_EF           (A_out_EF),
+
         .B_in               (B_in),
         .B_rden             (B_rden),
+        .B_in_EF            (B_in_EF),
         .B_out              (B_out),
         .B_wren             (B_wren),
+        .B_out_EF           (B_out_EF),
 
         .SIMD_A_in          (SIMD_A_in),
         .SIMD_A_rden        (SIMD_A_rden),
+        .SIMD_A_in_EF       (SIMD_A_in_EF),
         .SIMD_A_out         (SIMD_A_out),
         .SIMD_A_wren        (SIMD_A_wren),
+        .SIMD_A_out_EF      (SIMD_A_out_EF),
         
         .SIMD_B_in          (SIMD_B_in),
         .SIMD_B_rden        (SIMD_B_rden),
+        .SIMD_B_in_EF       (SIMD_B_in_EF),
         .SIMD_B_out         (SIMD_B_out),
-        .SIMD_B_wren        (SIMD_B_wren)
+        .SIMD_B_wren        (SIMD_B_wren),
+        .SIMD_B_out_EF      (SIMD_B_out_EF)
     );
 endmodule
 """)
@@ -162,7 +228,7 @@ endmodule
     parameters["assembler_base"]        = assembler_base
     return test_bench_template.substitute(parameters)
 
-def test_bench_script(parameters, default_bench = default_bench, SIMD_default_bench = SIMD_default_bench, install_base = install_base):
+def test_bench_script(parameters, default_bench = default_bench, SIMD_default_bench = SIMD_default_bench, install_base = install_base, quartus_base_path = quartus_base_path):
     test_bench_script_template = string.Template(
 """#! /bin/bash
 
@@ -170,13 +236,19 @@ INSTALL_BASE="${install_base}"
 
 TOP_LEVEL_MODULE="${CPU_NAME}_test_bench"
 TESTBENCH="./$${TOP_LEVEL_MODULE}.v"
-LOG_FILE="LOG_MODELSIM"
 
-LPM_LIBRARY="/pkgs/altera/quartus/quartus12.1/linux/quartus/eda/sim_lib/220model.v"
-ALT_LIBRARY="/pkgs/altera/quartus/quartus12.1/linux/quartus/eda/sim_lib/altera_mf.v"
+LPM_LIBRARY="${quartus_base_path}/linux/quartus/eda/sim_lib/220model.v"
+ALT_LIBRARY="${quartus_base_path}/linux/quartus/eda/sim_lib/altera_mf.v"
 
 OCTAVO="$$INSTALL_BASE/Octavo/Misc/params.v \\
         $$INSTALL_BASE/Octavo/Misc/delay_line.v \\
+        $$INSTALL_BASE/Octavo/Misc/Address_Decoder.v \\
+        $$INSTALL_BASE/Octavo/Misc/Address_Translator.v \\
+        $$INSTALL_BASE/Octavo/Misc/Addressed_Mux.v \\
+        $$INSTALL_BASE/Octavo/Misc/Translated_Addressed_Mux.v \\
+        $$INSTALL_BASE/Octavo/Misc/Instruction_Annuller.v \\
+        $$INSTALL_BASE/Octavo/Misc/Thread_Number.v \\
+        $$INSTALL_BASE/Octavo/Misc/Instr_Decoder.v \\
         $$INSTALL_BASE/Octavo/DataPath/ALU/AddSub_Carry_Select.v \\
         $$INSTALL_BASE/Octavo/DataPath/ALU/AddSub_Ripple_Carry.v \\
         $$INSTALL_BASE/Octavo/DataPath/ALU/Mult.v \\
@@ -184,18 +256,43 @@ OCTAVO="$$INSTALL_BASE/Octavo/Misc/params.v \\
         $$INSTALL_BASE/Octavo/DataPath/ALU/ALU.v \\
         $$INSTALL_BASE/Octavo/DataPath/DataPath.v \\
         $$INSTALL_BASE/Octavo/ControlPath/Controller.v \\
-        $$INSTALL_BASE/Octavo/ControlPath/Instr_Decoder.v \\
         $$INSTALL_BASE/Octavo/ControlPath/ControlPath.v \\
-        $$INSTALL_BASE/Octavo/Memory/Address_Decoder.v \\
-        $$INSTALL_BASE/Octavo/Memory/Address_Translator.v \\
-        $$INSTALL_BASE/Octavo/Memory/IO_Read_Data_Select.v \\
-        $$INSTALL_BASE/Octavo/Memory/IO_Read_Port_Rden.v \\
-        $$INSTALL_BASE/Octavo/Memory/IO_Read_Port_Select.v \\
-        $$INSTALL_BASE/Octavo/Memory/IO_Read_Port.v \\
-        $$INSTALL_BASE/Octavo/Memory/IO_Write_Port.v \\
         $$INSTALL_BASE/Octavo/Memory/RAM_SDP.v \\
+        $$INSTALL_BASE/Octavo/Memory/RAM_SDP_no_fw.v \\
         $$INSTALL_BASE/Octavo/Memory/Write_Enable.v \\
         $$INSTALL_BASE/Octavo/Memory/Memory.v \\
+        $$INSTALL_BASE/Octavo/Addressing/Address_Adder.v \\
+        $$INSTALL_BASE/Octavo/Addressing/Addressing_Mapped_AB.v \\
+        $$INSTALL_BASE/Octavo/Addressing/Addressing_Mapped_D.v \\
+        $$INSTALL_BASE/Octavo/Addressing/Addressing_Thread_Number.v \\
+        $$INSTALL_BASE/Octavo/Addressing/Addressing.v \\
+        $$INSTALL_BASE/Octavo/Addressing/Address_Translation.v \\
+        $$INSTALL_BASE/Octavo/Addressing/Default_Offset.v \\
+        $$INSTALL_BASE/Octavo/Addressing/Increment_Adder.v \\
+        $$INSTALL_BASE/Octavo/Addressing/Increments.v \\
+        $$INSTALL_BASE/Octavo/Addressing/Programmed_Offsets.v \\
+        $$INSTALL_BASE/Octavo/Addressing/Write_Priority.v \\
+        $$INSTALL_BASE/Octavo/Addressing/Write_Synchronize.v \\
+        $$INSTALL_BASE/Octavo/Branching/Branch_Check_Mapped.v \\
+        $$INSTALL_BASE/Octavo/Branching/Branch_Check.v \\
+        $$INSTALL_BASE/Octavo/Branching/Branch_Condition.v \\
+        $$INSTALL_BASE/Octavo/Branching/Branch_Destination.v \\
+        $$INSTALL_BASE/Octavo/Branching/Branch_Folding.v \\
+        $$INSTALL_BASE/Octavo/Branching/Branching_Flags.v \\
+        $$INSTALL_BASE/Octavo/Branching/Branching_Thread_Number.v \\
+        $$INSTALL_BASE/Octavo/Branching/Branch_Origin.v \\
+        $$INSTALL_BASE/Octavo/Branching/Branch_Origin_Check.v \\
+        $$INSTALL_BASE/Octavo/Branching/Branch_Cancel.v \\
+        $$INSTALL_BASE/Octavo/Branching/Branch_Prediction.v \\
+        $$INSTALL_BASE/Octavo/Branching/Branch_Prediction_Enable.v \\
+        $$INSTALL_BASE/Octavo/Branching/OR_Reducer.v \\
+        $$INSTALL_BASE/Octavo/IO/EmptyFullBit.v \\
+        $$INSTALL_BASE/Octavo/IO/IO_Active.v \\
+        $$INSTALL_BASE/Octavo/IO/IO_All_Ready.v \\
+        $$INSTALL_BASE/Octavo/IO/IO_Check.v \\
+        $$INSTALL_BASE/Octavo/IO/IO_Read.v \\
+        $$INSTALL_BASE/Octavo/IO/IO_Write.v \\
+        $$INSTALL_BASE/Octavo/IO/Port_Active.v \\
         $$INSTALL_BASE/Octavo/Octavo/Scalar.v \\
         $$INSTALL_BASE/Octavo/Octavo/SIMD.v \\
         ../${CPU_NAME}.v \\
@@ -206,15 +303,16 @@ VLIB="work"
 VSIM_ACTIONS="vcd file $$TOP_LEVEL_MODULE.vcd ; vcd add -r /* ; run -all ; quit"
 
 rm $$TOP_LEVEL_MODULE.wlf $$TOP_LEVEL_MODULE.vcd
-vlib $$VLIB 2>&1 > $$LOG_FILE
-vlog -mfcu -incr -lint $$LPM_LIBRARY $$ALT_LIBRARY $$OCTAVO $$TESTBENCH 2>&1 > $$LOG_FILE
-vsim -voptargs="+acc" -c -do "$$VSIM_ACTIONS" $$TOP_LEVEL_MODULE 2>&1 > $$LOG_FILE
-vcd2wlf $$TOP_LEVEL_MODULE.vcd $$TOP_LEVEL_MODULE.wlf 2>&1 > $$LOG_FILE
+vlib $$VLIB 2>&1 > LOG
+vlog -mfcu -incr -lint $$LPM_LIBRARY $$ALT_LIBRARY $$OCTAVO $$TESTBENCH 2>&1 > LOG
+vsim -voptargs="+acc" -c -do "$$VSIM_ACTIONS" $$TOP_LEVEL_MODULE 2>&1 > LOG
+vcd2wlf $$TOP_LEVEL_MODULE.vcd $$TOP_LEVEL_MODULE.wlf 2>&1 > LOG
 rm vsim.wlf
 """)
     parameters["default_bench"]         = default_bench
     parameters["SIMD_default_bench"]    = SIMD_default_bench
     parameters["install_base"]          = install_base
+    parameters["quartus_base_path"]     = quartus_base_path
     return test_bench_script_template.substitute(parameters)
 
 def main(parameters = {}):
