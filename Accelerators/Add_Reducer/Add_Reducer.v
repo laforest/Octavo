b2b1fb5e416b06494de298ec4bd5d3df1afdab55
Charles Eric LaForest <laforest@eecg.utoronto.ca>
2014-05-18 20:53:48 -0400

    Add_Reducer tested. Simple test bench and synthesis setup.
    Serious unexplained behaviour. See comments.
    Initial support for Icarus Verilog. (I had to compare with Modelsim.)

--- Accelerators/Add_Reducer/Add_Reducer.v
+++ Accelerators/Add_Reducer/Add_Reducer.v
@@ -4,25 +4,33 @@
 
 module Add_Reducer
 #(
-    parameter WORD_WIDTH = 0,
+    parameter WORD_WIDTH = 36,
     parameter ADDENDS    = 8  // ECL XXX Hardcoded for now
 )
 (
-    input   wire                                            clock,
-	input   wire    signed  [(ADDENDS * WORD_WIDTH)-1:0]    addends,
-	output  reg     signed  [WORD_WIDTH-1:0]                reduction
+    input   wire                                    clock,
+	input   wire    [(ADDENDS * WORD_WIDTH)-1:0]    addends,
+	output  wire    [WORD_WIDTH-1:0]                reduction
 );
 
 // -----------------------------------------------------------
+
+// XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+// ECL XXX FIXME I have no idea why:
+// ECL XXX FIXME The first instance always has DEPTH-1 or 0 for no reason.
+// ECL XXX FIXME This 0-DEPTH instance solely exists to contain the bug
+// ECL XXX FIXME which manifests in both Modelsim and Icarus Verilog
+// ECL XXX FIXME but not Quartus.
+// XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
 
     wire [(ADDENDS * WORD_WIDTH)-1:0] addends_delayed;
 
     delay_line 
     #(
-        .DEPTH  (1),
-        .WIDTH  (ADDENDS * WORD_WIDTH)
+        .DEPTH  (0),
+        .WIDTH  (WORD_WIDTH)
     ) 
-    addends_pipeline
+    addends_pipeline [ADDENDS-1:0]
     (
         .clock  (clock),
         .in     (addends),
@@ -31,7 +39,23 @@ module Add_Reducer
 
 // -----------------------------------------------------------
 
-    wire [((ADDENDS/2) * WORD_WIDTH))-1] partial_reduction_1;
+    wire [(ADDENDS * WORD_WIDTH)-1:0] addends_delayed_2;
+
+    delay_line 
+    #(
+        .DEPTH  (1),
+        .WIDTH  (WORD_WIDTH)
+    ) 
+    addends_pipeline_2 [ADDENDS-1:0]
+    (
+        .clock  (clock),
+        .in     (addends_delayed),
+        .out    (addends_delayed_2)
+    );
+
+// -----------------------------------------------------------
+
+    wire [((ADDENDS/2) * WORD_WIDTH)-1:0] partial_reduction_1;
 
     AddSub_Ripple_Carry 
     #(
@@ -42,15 +66,15 @@ module Add_Reducer
         .clock      (clock),
         .add_sub    (`HIGH),
         .cin        (`LOW),
-        .dataa      (addends_delayed [0 +: ((ADDENDS/2) * WORD_WIDTH)]),
-        .datab      (addends_delayed [((ADDENDS/2) * WORD_WIDTH) +: ((ADDENDS/2) * WORD_WIDTH)]),
+        .dataa      (addends_delayed_2 [0 +: ((ADDENDS/2) * WORD_WIDTH)]),
+        .datab      (addends_delayed_2 [((ADDENDS/2) * WORD_WIDTH) +: ((ADDENDS/2) * WORD_WIDTH)]),
         .cout       (),
         .result     (partial_reduction_1)
     );
 
 // -----------------------------------------------------------
 
-    wire [((ADDENDS/4) * WORD_WIDTH))-1] partial_reduction_2;
+    wire [((ADDENDS/4) * WORD_WIDTH)-1:0] partial_reduction_2;
 
     AddSub_Ripple_Carry 
     #(
@@ -69,7 +93,7 @@ module Add_Reducer
 
 // -----------------------------------------------------------
 
-    wire [((ADDENDS/8) * WORD_WIDTH))-1] partial_reduction_3;
+    wire [((ADDENDS/8) * WORD_WIDTH)-1:0] partial_reduction_3;
 
     AddSub_Ripple_Carry 
     #(
