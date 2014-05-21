41ac549dcf14676514887d17c77d19e43d606426
Charles Eric LaForest <laforest@eecg.utoronto.ca>
2014-05-21 13:01:30 -0400

    Reversed order of Indirect and Shared Memory mappings in Addressing.
    I/O ports now override Indirect memory.
    Cleaner, avoids muddled cases where they overlapped.
    Improves flexibility since I/O mapping is local to Scalar/SIMD lanes.

--- Octavo/Addressing/Addressing.v
+++ Octavo/Addressing/Addressing.v
@@ -366,8 +366,10 @@ module Addressing
 
 // -----------------------------------------------------------
 
-    // output default offset, unless we access shared hardware, then output 0
-    wire    [DEFAULT_OFFSET_WORD_WIDTH-1:0]    default_offset_final;
+    // output default offset, 
+    // unless we access indirect memory, 
+    // else output programmed offset
+    wire    [DEFAULT_OFFSET_WORD_WIDTH-1:0]    selected_offset;
 
     Addressed_Mux
     #(
@@ -376,21 +378,19 @@ module Addressing
         .INPUT_COUNT    (2),
         .REGISTERED     (`FALSE)
     )
-    default_offset_selector
+    offset_selector
     (
         .clock          (clock),
-        .addr           (shared_hardware_memory),
-        .data_in        ({{DEFAULT_OFFSET_WORD_WIDTH{`LOW}}, default_offset}),
-        .data_out       (default_offset_final)
+        .addr           (indirect_memory),
+        .data_in        ({programmed_offset, default_offset}),
+        .data_out       (selected_offset)
     );
 
 // -----------------------------------------------------------
 
-    // ECL XXX Not that default and programmed should ever differ in width.
-    // If we access indirect memory, then override default offset with programmed offset
-    // This causes odd cases if indirect memory overlaps shared hardware like I/O ports.
-    // Usable, but think twice about it first.
-    // if PO/INC are 0, I/O works as usual, else gets redirected to RAM, incl. other I/O port!)
+    // Shared Hardware (e.g. I/O ports) override any other mapping.
+    // This removes ambiguity (altering a port address mid-I/O pipeline)
+    // and increases flexibility since I/O mappings are separate across Scalar/SIMD lanes
     wire    [DEFAULT_OFFSET_WORD_WIDTH-1:0]    final_offset;
 
     Addressed_Mux
@@ -400,11 +400,11 @@ module Addressing
         .INPUT_COUNT    (2),
         .REGISTERED     (`TRUE)
     )
-    PO_DO_selector
+    shared_hardware_selector
     (
         .clock          (clock),
-        .addr           (indirect_memory),
-        .data_in        ({programmed_offset, default_offset_final}),
+        .addr           (shared_hardware_memory),
+        .data_in        ({{DEFAULT_OFFSET_WORD_WIDTH{`LOW}}, selected_offset}),
         .data_out       (final_offset)
     );
 
