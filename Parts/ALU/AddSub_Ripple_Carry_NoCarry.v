17a0d2b448dba96c8f2c5d063dfc064a7db3003e
Charles Eric LaForest <laforest@eecg.utoronto.ca>
2016-07-08 11:09:34 -0400

    Add overflow and carry-out to ALU adder
    Turns out, Quartus synthesizes this quite well.
    It retimes output registers back into the adder.

--- Parts/ALU/AddSub_Ripple_Carry_NoCarry.v
+++ Parts/ALU/AddSub_Ripple_Carry_NoCarry.v
@@ -3,6 +3,8 @@
 // This allows us to compute +/-A+/-B.
 // Should infer to the usual LUT and carry-chain logic.
 
+// Also generates addition-specific predicates for later comparisons
+
 module AddSub_Ripple_Carry_NoCarry
 #(
     parameter               WORD_WIDTH          = 0
@@ -12,10 +14,66 @@ module AddSub_Ripple_Carry_NoCarry
     input   wire                                A_negative,
     input   wire    signed  [WORD_WIDTH-1:0]    B,
     input   wire                                B_negative,
-    output  reg     signed  [WORD_WIDTH-1:0]    sum
+    output  reg     signed  [WORD_WIDTH-1:0]    sum,
+    output  reg                                 carry_out,
+    output  reg                                 overflow
 );
+
+// --------------------------------------------------------------------
+
+    wire [WORD_WIDTH-1:0] A_signed;
+    wire [WORD_WIDTH-1:0] B_signed;
+
+    Inverter
+    #(
+        .WORD_WIDTH (WORD_WIDTH)
+    )
+    A_inv
+    (
+        .invert     (A_negative),
+        .in         (A),
+        .out        (A_signed)
+    );
+
+    Inverter
+    #(
+        .WORD_WIDTH (WORD_WIDTH)
+    )
+    B_inv
+    (
+        .invert     (B_negative),
+        .in         (B),
+        .out        (B_signed)
+    );
+
+    // -X = (~X)+1
+
     always @(*) begin
-        sum <= (A ^ {WORD_WIDTH{A_negative}}) + (B ^ {WORD_WIDTH{B_negative}}) + A_negative + B_negative;
+        {carry_out,sum} <= A_signed + B_signed + A_negative + B_negative;
     end
+
+// --------------------------------------------------------------------
+// Find carry-in into MSB
+
+    wire carry_in;
+
+    Carryin_Calculator
+    #(
+        .WORD_WIDTH (1)
+    )
+    MSB_Cin
+    (
+        .A          (A_signed[WORD_WIDTH-1]),
+        .B          (B_signed[WORD_WIDTH-1]),
+        .S          (sum     [WORD_WIDTH-1]),
+        .Cin        (carry_in)
+    );
+
+// --------------------------------------------------------------------
+
+    always @(*) begin
+        overflow = carry_in ^ carry_out; // signed overflow
+    end
+
 endmodule
 
