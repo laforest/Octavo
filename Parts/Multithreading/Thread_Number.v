26167ce448b5da0659f1cc87b9e545e892b8fe1e
Charles Eric LaForest <laforest@eecg.utoronto.ca>
2016-07-11 10:25:24 -0400

    Simplify Thread_Number using UpDown_counter.

--- Parts/Multithreading/Thread_Number.v
+++ Parts/Multithreading/Thread_Number.v
@@ -11,36 +11,41 @@
 
 module Thread_Number
 #(
-    parameter   INITIAL_THREAD              = 0,
-    parameter   THREAD_COUNT                = 0,
-    parameter   THREAD_ADDR_WIDTH           = 0
+    parameter   INITIAL_THREAD                  = 0,
+    parameter   THREAD_COUNT                    = 0,
+    parameter   THREAD_COUNT_WIDTH              = 0
 )
 (
-    input   wire                            clock,
-    output  reg     [THREAD_ADDR_WIDTH-1:0] current_thread,
-    output  reg     [THREAD_ADDR_WIDTH-1:0] next_thread
+    input   wire                                clock,
+    output  wire    [THREAD_COUNT_WIDTH-1:0]    current_thread,
+    output  wire    [THREAD_COUNT_WIDTH-1:0]    next_thread
 );
 
-    // Workaround to allow bit vector selection to eliminate truncation warnings
-    // Cause: can't put a parameter in the bitwidth part of a literal.
-    // e.g.: THREAD_ADDR_WIDTH'd1 is not allowed. :P
-    integer zero = 0;
-    integer one  = 1;
-
-    initial begin
-        current_thread = INITIAL_THREAD[THREAD_ADDR_WIDTH-1:0];
-    end
-
-    reg last_thread;
+    // Avoids width-mismatch warnings
+    localparam ZERO = {THREAD_COUNT_WIDTH{1'b0}};
 
     always @(*) begin
         // Doing it this way to avoid an adder/subtracter comparator.
         last_thread = (current_thread == (THREAD_COUNT-1));
-        next_thread = !last_thread ? current_thread + one[THREAD_ADDR_WIDTH-1:0] : zero[THREAD_ADDR_WIDTH-1:0];
     end
 
-    always @(posedge clock) begin
-        current_thread <= next_thread;
-    end
+// --------------------------------------------------------------------
+
+    UpDown_Counter
+    #(
+        .WORD_WIDTH     (THREAD_COUNT_WIDTH),
+        .INITIAL_COUNT  (INITIAL_THREAD)
+    )
+    Thread
+    (
+        .clock          (clock),
+        .up_down        (1'b1),         // 1/0 up/down
+        .run            (1'b1),         // counts one step if set
+        .wren           (last_thread),  // Write overrules count
+        .write_data     (ZERO),
+        .count          (current_thread),
+        .next_count     (next_thread)
+    );
+
 endmodule
 
