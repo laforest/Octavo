ccbc344316e24049ffc15b1cfcc740e53840df5f
Charles Eric LaForest <laforest@eecg.utoronto.ca>
2016-07-12 10:28:20 -0400

    Rename Masked_Match to Sentinel_Value_Check

--- Parts/Misc/Sentinel_Value_Check.v
+++ Parts/Misc/Sentinel_Value_Check.v
@@ -1,16 +1,16 @@
 
-// Checks if the input matches the target value, 
+// Checks if the input matches the sentinel value, 
 // with masking to exclude bits from match.
 
 // Set mask to 0 to exclude a bit from the match.
 
-module Masked_Match
+module Sentinel_Value_Check
 #(
     parameter WORD_WIDTH = 0
 )
 (
     input   wire    [WORD_WIDTH-1:0]    in,
-    input   wire    [WORD_WIDTH-1:0]    target, 
+    input   wire    [WORD_WIDTH-1:0]    sentinel, 
     input   wire    [WORD_WIDTH-1:0]    mask,
     output  reg                         match
 );
@@ -18,7 +18,7 @@ module Masked_Match
     reg [WORD_WIDTH-1:0] raw_match;
 
     always @(*) begin
-        raw_match = ~(in ^ target) & mask;
+        raw_match = ~(in ^ sentinel) & mask;
         match     = | raw_match;
     end
 
