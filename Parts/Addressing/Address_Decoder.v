75f27e39be3336b53785bd365019e763865e816b
Charles Eric LaForest <laforest@eecg.utoronto.ca>
2016-08-05 22:37:01 -0400

    Check only valid address range in Address_Decoder
    This speeds up synthesis massively, but does not solve the O(n^2) scaling.

--- Parts/Addressing/Address_Decoder.v
+++ Parts/Addressing/Address_Decoder.v
@@ -1,20 +1,24 @@
 
 // A *universal* address decoder. Works for any address range at any starting point.
 
-// Checks if the input address matches each possible address in range, then
-// outputs the bitwise OR of all these checks.  Some boolean algebra shows that
-// it will always optimize down to a minimal form: any bits which iterate over
-// their entire binary range become boolean "don't care", leaving the other
-// bits to do the match. Thus, for aligned power of 2 address ranges, we get
-// the minimal NOT-AND-gate decoder.
+// Checks if the input address matches each possible address in the range,
+// then outputs the bitwise OR of all these checks.  Some boolean algebra
+// shows that it will always optimize down to a minimal form: any bits which
+// iterate over their entire binary range become boolean "don't care", leaving
+// the other bits to do the match. Thus, for aligned power of 2 address
+// ranges, we get the minimal NOT-AND-gate decoder.
 
-// This approach has one caveat: you have to test, at synthesis time, all 2^N
-// possible addresses, and store the matches into a vector 2^N bits long.  This
-// could take a long time and AFAIK, Verilog implementations have a maximum
-// vector width of a few million, so this decoder will break for addresses more
-// than 20-23 bits wide.
+// This approach has one caveat: you may have to test, at synthesis time, up
+// to all 2^N possible addresses, and store the matches into a vector 2^N bits
+// long.  This could take a long time and AFAIK, Verilog implementations have
+// a maximum vector width of a few million, so this decoder will break for
+// address ranges more than 20-23 bits wide.
 
 // Call it a synthesizer stress-test. ;)
+
+// Even if your CAD tool can handle huge vectors, because we use an int as
+// counter, this decoder cannot be guaranteed to work for address ranges
+// exceeding 32 bits.
 
 module Address_Decoder
 #(
@@ -26,13 +30,15 @@ module Address_Decoder
     input   wire    [ADDR_WIDTH-1:0]    addr,
     output  reg                         hit 
 );
+    localparam ADDR_BOUND = ADDR_BASE + ADDR_COUNT;
+
     integer                     i;
     reg     [ADDR_COUNT-1:0]    per_addr_match = 0;
 
     // Check each address in range for match
     always @(*) begin
-        for(i = 0; i < ADDR_COUNT; i = i + 1) begin : addr_decode
-            per_addr_match[i] <= (addr == (ADDR_BASE + i));
+        for(i = ADDR_BASE; i < ADDR_BOUND; i = i + 1) begin : addr_decode
+            per_addr_match[i] <= (addr == i);
         end
     end
 
