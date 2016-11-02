02798544f43bdd5635674808d739453d171e4728
Charles Eric LaForest <laforest@eecg.utoronto.ca>
2016-11-02 13:16:47 -0400

    Clean up sentinel value check.
    Match default FPGA init behaviour
    Clarify that it's an ALL-bits-match check.
    Use multiple units in parallel for an ANY-bits-match check.

--- Parts/Misc/Sentinel_Value_Check.v
+++ Parts/Misc/Sentinel_Value_Check.v
@@ -1,15 +1,16 @@
 
-// Checks if the input matches the sentinel value, 
+// Checks if ALL the input bits match the sentinel value, 
 // with masking to exclude bits from match.
 
-// Set a mask bit to 0 to exclude a bit from the comparison.
+// Set a mask bit to 1 to exclude a bit from the comparison.
 // (by forcing both the input and sentinel bit to zero, thus matching.)
-// Thus, an all-zero mask will cause a match to always be found.
-// And an all-one mask will cause a comparison to be exact.
+// Thus, an all-one mask will cause a match to always be found.
+// And an all-zero mask will cause a comparison to be exact.
+// (which will be the default behaviour at start)
 
 module Sentinel_Value_Check
 #(
-    parameter WORD_WIDTH = 0
+    parameter       WORD_WIDTH          = 0
 )
 (
     input   wire    [WORD_WIDTH-1:0]    in,
@@ -18,13 +19,17 @@ module Sentinel_Value_Check
     output  reg                         match
 );
 
-    reg [WORD_WIDTH-1:0] raw_match;
-    reg [WORD_WIDTH-1:0] masked_in;
-    reg [WORD_WIDTH-1:0] masked_sentinel;
+    initial begin
+        match = 0;
+    end
+
+    reg [WORD_WIDTH-1:0] masked_in          = 0;
+    reg [WORD_WIDTH-1:0] masked_sentinel    = 0;
+    reg [WORD_WIDTH-1:0] raw_match          = 0;
 
     always @(*) begin
-        masked_in       = in       & mask;
-        masked_sentinel = sentinel & mask;
+        masked_in       = in       & ~mask;
+        masked_sentinel = sentinel & ~mask;
         raw_match       = ~(masked_in ^ masked_sentinel);
         match           = & raw_match;
     end
