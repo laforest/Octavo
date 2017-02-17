84c4181b83846292f99f2d57dd7d1b6670058033
Charles Eric LaForest <eric.laforest@gmail.com>
2017-02-17 09:58:01 -0500

    Revert back Branch Counter to plain down-counter, and remove jump condition
    This was a bad design decision, as the jump selection is not necessary,
    and the code was bad, full of random logic uncontained by a module.
    This is not tested yet, but is much cleaner and simpler.

--- Parts/Branching/Branch_Counter.v
+++ Parts/Branching/Branch_Counter.v
@@ -1,18 +1,18 @@
 
-// Branch Counter: once loaded, counts down to zero based on a selectable
-// condition: When the branch has been reached, decrement either if the
-// branch was taken or not.
+// Branch Counter: once loaded, counts down one step to zero each time the
+// branch is reached.
 
-// If the counter is at zero, it will raise "done" once, and clear its
-// internal "run" bit, halting the counter, clearing "done", and leaving its
-// memory word all-zero.
-// Thus, for a counter value of N, "done" will be raised on the N+1 pass.
+// If the counter is at zero, it will halt counting and raise it's "done"
+// output and hold it there until it is reloaded.
 
-// To restart the counter, you must reload the counter with a new count value
-// and a new condition. The "run" bit is internally set by the configuration write. 
+// So for a counter value of N, "done" will seen on the N+1 pass.
 
-// TODO: If the counter is too slow: pipeline the counter. Delay write thread
-// number as required.
+// To restart the counter, you must reload the counter with a new count value.
+// Load overrides a coincident decrement.
+
+// Counter starts at zero at initialization.
+
+// Multithreaded: one counter per thread, one each clock cycle.
 
 module Branch_Counter
 #(
@@ -22,86 +22,36 @@ module Branch_Counter
 (
     input   wire                        clock,
     input   wire                        branch_reached,
-    input   wire                        branch_taken,
-    input   wire                        configuration_wren,
-    input   wire    [WORD_WIDTH-1:0]    configuration_data,
-    output  reg                         done
+    input   wire                        start_count_wren,
+    input   wire    [WORD_WIDTH-1:0]    start_count,
+    output  wire                        done
 );
 
-    initial begin
-        done = 0;
-    end
-
-    localparam RUN_WIDTH        = 1;
-    localparam TAKEN_WIDTH      = 1;
-    localparam COUNTER_WIDTH    = WORD_WIDTH - TAKEN_WIDTH;
-    // One larger than WORD_WIDTH
-    localparam MEM_WIDTH        = COUNTER_WIDTH + TAKEN_WIDTH + RUN_WIDTH;
+// --------------------------------------------------------------------
 
     localparam COUNTER_ZERO     = {COUNTER_WIDTH{1'b0}};
     localparam COUNTER_ONE      = {{(COUNTER_WIDTH-1){1'b0}},1'b1};
 
 // --------------------------------------------------------------------
 
-    // Define the counter data structure in a memory word.
+    wire [WORD_WIDTH-1:0]   current_count;
+    wire [WORD_WIDTH-1:0]   new_count;
+    wire                    new_count_wren;
 
-    wire    [MEM_WIDTH-1:0]     mem_read_data;
-    reg     [COUNTER_WIDTH-1:0] count           = 0
-    reg                         taken           = 0;
-    reg                         run             = 0;
-
-    always @(*) begin
-        {run,count,taken} = mem_read_data;
-    end
-
-// --------------------------------------------------------------------
-
-    // The configuration data of course must have the same structure.
-    // Note the dropped most-significant bit!!!
-    // That bit contains the internal run bit.
-
-    reg     [COUNTER_WIDTH-1:0]  config_count   = 0
-    reg                          config_taken   = 0;
-
-    always @(*) begin
-        {config_count,config_taken} = configuration_data;
-    end
-
-// --------------------------------------------------------------------
-
-    // And write the same structure back to memory.
-
-    reg     [MEM_WIDTH-1:0]     mem_write_data  = 0;
-    reg     [COUNTER_WIDTH-1:0] mem_count       = 0;
-    reg                         mem_taken       = 0;
-    reg                         mem_run         = 0;
-
-    always @(*) begin
-        mem_write_data = {mem_run,mem_count,mem_taken};
-    end
-
-// --------------------------------------------------------------------
-
-    // Now select what to write to memory: the decremented count and its
-    // condition and run bits, or the new configuration data.
-    // Note the automatically set run bit on configuration write.
-
-    reg                     count_is_zero   = 0;
-    reg [COUNTER_WIDTH-1:0] new_count       = 0;
-    reg                     mem_wren        = 0;
-
-    always @(*) begin
-        count_is_zero = (count == COUNTER_ZERO);
-        new_count     = count - COUNTER_ONE;
-
-        {mem_run,mem_count,mem_taken} = (count_is_zero      == 1'b1) ? {1'b0,COUNTER_ZERO,1'b0}         : {run,new_count,taken};
-        {mem_run,mem_count,mem_taken} = (configuration_wren == 1'b1) ? {1'b1,config_count,config_taken} : {mem_run,mem_count,mem_taken};
-
-        mem_wren = (branch_reached == 1'b1) & (branch_taken == taken);
-        mem_wren = (configuration_wren == 1'b1) ? 1'b1 : mem_wren;;
-
-        done = (count_is_zero == 1'b1) & (run == 1'b1); 
-    end
+    Down_Counter_Zero
+    #(
+        .WORD_WIDTH         (WORD_WIDTH)
+    )
+    BC_DCZ
+    (
+        .run                (branch_reached),
+        .count_in           (current_count),
+        .load_wren          (start_count_wren),
+        .load_value         (start_count),
+        .count_out_wren     (new_count_wren),
+        .count_out          (new_count),
+        .zero               (done)
+    );
 
 // --------------------------------------------------------------------
 
@@ -127,12 +77,12 @@ module Branch_Counter
 
 // --------------------------------------------------------------------
 
-    // Store the count value, run, and condition bits
+    // Store the count value.
     // One word per thread.
 
     RAM_SDP 
     #(
-        .WORD_WIDTH     (MEM_WIDTH),
+        .WORD_WIDTH     (WORD_WIDTH),
         .ADDR_WIDTH     (`OCTAVO_THREAD_COUNT_WIDTH),
         .DEPTH          (`OCTAVO_THREAD_COUNT),
         .RAMSTYLE       (RAMSTYLE),
@@ -143,12 +93,12 @@ module Branch_Counter
     BC_RAM
     (
         .clock          (clock),
-        .wren           (mem_wren),
+        .wren           (new_count_wren),
         .write_addr     (thread_number_write),
-        .write_data     (mem_write_data),
+        .write_data     (new_count),
         .rden           (1'b1),
         .read_addr      (thread_number_read), 
-        .read_data      (mem_read_data)
+        .read_data      (current_count)
     );
 
 endmodule
