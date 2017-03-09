cf9654320c44cde9ebba44ba9e9ad2970b17084c
Charles Eric LaForest <eric.laforest@gmail.com>
2017-03-09 12:48:29 -0500

    Re-do Instruction Field Extractor using assignments to concatenations
    Much simpler and cleaner.
    Also add split addressing operands.

--- Parts/Misc/Instruction_Field_Extractor.v
+++ Parts/Misc/Instruction_Field_Extractor.v
@@ -1,6 +1,6 @@
 
-// Pulls out each field out of an instruction.
-// Format: OP:D:A:B, with any spare bits before B in the LSB positions.
+// Extracts each field out of an instruction,
+// with any unused bits in the MSB before the opcode.
 
 module Instruction_Field_Extractor 
 #(
@@ -8,20 +8,36 @@ module Instruction_Field_Extractor
     parameter       OPCODE_WIDTH            = 0,
     parameter       D_OPERAND_WIDTH         = 0,
     parameter       A_OPERAND_WIDTH         = 0,
-    parameter       B_OPERAND_WIDTH         = 0
+    parameter       B_OPERAND_WIDTH         = 0,
+    // Don't set at instantiation
+    // Assumes D_OPERAND_WIDTH is even
+    parameter       D_SPLIT_WIDTH           = D_OPERAND_WIDTH / 2
 )
 (
     input   wire    [INSTRUCTION_WIDTH-1:0] instruction,
     output  reg     [OPCODE_WIDTH-1:0]      opcode,
-    output  reg     [D_OPERAND_WIDTH-1:0]   D_operand,   // Destination 
-    output  reg     [A_OPERAND_WIDTH-1:0]   A_operand,   // Source
-    output  reg     [B_OPERAND_WIDTH-1:0]   B_operand    // Source
+    output  reg     [D_OPERAND_WIDTH-1:0]   D_operand,      // Destination 
+    output  reg     [D_SPLIT_WIDTH-1:0]     D_split_lower,  // Split addressing mode
+    output  reg     [D_SPLIT_WIDTH-1:0]     D_split_upper,
+    output  reg     [A_OPERAND_WIDTH-1:0]   A_operand,      // Source
+    output  reg     [B_OPERAND_WIDTH-1:0]   B_operand       // Source
 );
-    always @(*) begin
-        opcode      <= instr[(INSTR_WIDTH-1)                                                        : (D_OPERAND_WIDTH + A_OPERAND_WIDTH + B_OPERAND_WIDTH)];
-        D_operand   <= instr[(INSTR_WIDTH - OPCODE_WIDTH - 1)                                       : (A_OPERAND_WIDTH + B_OPERAND_WIDTH)];
-        A_operand   <= instr[(INSTR_WIDTH - OPCODE_WIDTH - D_OPERAND_WIDTH - 1)                     : B_OPERAND_WIDTH];
-        B_operand   <= instr[(INSTR_WIDTH - OPCODE_WIDTH - D_OPERAND_WIDTH - A_OPERAND_WIDTH - 1)   : 0];
+
+    initial begin
+        opcode          = 0;
+        D_operand       = 0;
+        D_split_lower   = 0;
+        D_split_upper   = 0;
+        A_operand       = 0;
+        B_operand       = 0;
     end
+
+    localparam USED_BIT_WIDTH = OPCODE_WIDTH + D_OPERAND_WIDTH + A_OPERAND_WIDTH + B_OPERAND_WIDTH;
+
+    always @(*) begin
+        {opcode,D_operand,A_operand,B_operand}  = instruction [USED_BIT_WIDTH-1:0];
+        {D_split_upper,D_split_lower}           = D_operand;
+    end
+
 endmodule
 
