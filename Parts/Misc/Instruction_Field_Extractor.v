deffa178ebf7dc52e3cb2aa3c840211c898328e3
Charles Eric LaForest <eric.laforest@gmail.com>
2017-09-01 06:36:17 -0400

    Remove split D from instruction field extractor.
    D splitting left to Address Splitter module.

--- Parts/Misc/Instruction_Field_Extractor.v
+++ Parts/Misc/Instruction_Field_Extractor.v
@@ -2,23 +2,21 @@
 // Extracts each field out of an instruction,
 // with any unused bits in the MSB before the opcode.
 
+// Documents the overall intruction format.
+// See Address_Splitter for the split D address format.
+
 module Instruction_Field_Extractor 
 #(
-    parameter       INSTRUCTION_WIDTH       = 0,
+    parameter       WORD_WIDTH              = 0,
     parameter       OPCODE_WIDTH            = 0,
     parameter       D_OPERAND_WIDTH         = 0,
     parameter       A_OPERAND_WIDTH         = 0,
-    parameter       B_OPERAND_WIDTH         = 0,
-    // Don't set at instantiation
-    // Assumes D_OPERAND_WIDTH is even, else MSB dropped
-    parameter       D_SPLIT_WIDTH           = D_OPERAND_WIDTH / 2
+    parameter       B_OPERAND_WIDTH         = 0
 )
 (
-    input   wire    [INSTRUCTION_WIDTH-1:0] instruction,
+    input   wire    [WORD_WIDTH-1:0]        instruction,
     output  reg     [OPCODE_WIDTH-1:0]      opcode,
     output  reg     [D_OPERAND_WIDTH-1:0]   D_operand,      // Destination 
-    output  reg     [D_SPLIT_WIDTH-1:0]     D_split_lower,  // Split addressing mode
-    output  reg     [D_SPLIT_WIDTH-1:0]     D_split_upper,
     output  reg     [A_OPERAND_WIDTH-1:0]   A_operand,      // Source
     output  reg     [B_OPERAND_WIDTH-1:0]   B_operand       // Source
 );
@@ -26,8 +24,6 @@ module Instruction_Field_Extractor
     initial begin
         opcode          = 0;
         D_operand       = 0;
-        D_split_lower   = 0;
-        D_split_upper   = 0;
         A_operand       = 0;
         B_operand       = 0;
     end
@@ -36,7 +32,6 @@ module Instruction_Field_Extractor
 
     always @(*) begin
         {opcode,D_operand,A_operand,B_operand}  = instruction [USED_BIT_WIDTH-1:0];
-        {D_split_upper,D_split_lower}           = D_operand   [(D_SPLIT_WIDTH*2)-1:0];
     end
 
 endmodule
