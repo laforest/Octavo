3c56855e3924579564b8eb67da4b753a60c8c916
Charles Eric LaForest <eric.laforest@gmail.com>
2017-09-13 16:29:41 -0400

    Add annulled/cancelled read/write enable for memory addressing.

--- Parts/Address_Decode/Memory_Addressing.v
+++ Parts/Address_Decode/Memory_Addressing.v
@@ -13,11 +13,33 @@ module Memory_Addressing
     parameter   MEM_WRITE_BOUND_ADDR        = 0
 )
 (
+    // From current instruction reading memory
+    input   wire                            IOR,
+    input   wire                            cancel,
     input   wire    [READ_ADDR_WIDTH-1:0]   read_addr,
-    input   wire    [WRITE_ADDR_WIDTH-1:0]  write_addr,
     output  wire                            read_enable,
+
+    // From previous instruction about to write to memory
+    input   wire                            IOR_previous,
+    input   wire                            cancel_previous,
+    input   wire    [WRITE_ADDR_WIDTH-1:0]  write_addr,
     output  wire                            write_enable
 );
+
+
+// --------------------------------------------------------------------
+
+    // Enable reads/writes only if the instruction performing the read/write
+    // was not annulled nor cancelled. 
+
+    reg read_instruction_ok  = 0;
+    reg write_instruction_ok = 0;
+
+    always @(*) begin
+        read_instruction_ok  <= (IOR          == 1'b1) & (cancel          == 1'b0);
+        write_instruction_ok <= (IOR_previous == 1'b1) & (cancel_previous == 1'b0);
+    end
+
 // --------------------------------------------------------------------
 
     Address_Range_Decoder_Static
@@ -28,7 +50,7 @@ module Memory_Addressing
     )
     Read
     (
-        .enable     (1'b1),
+        .enable     ((read_instruction_ok == 1'b1)),
         .addr       (read_addr),
         .hit        (read_enable)
     );
@@ -43,7 +65,7 @@ module Memory_Addressing
     )
     Write
     (
-        .enable     (1'b1),
+        .enable     ((write_instruction_ok == 1'b1)),
         .addr       (write_addr),
         .hit        (write_enable)
     );
