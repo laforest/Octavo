a1f300dc2b3ce41a7761c4e3cbca99dd0cd3cb44
Charles Eric LaForest <eric.laforest@gmail.com>
2017-11-08 12:55:34 -0500

    Add tracing to Verilator simulation.

--- Tests/Octavo/test_bench/test_bench.cpp
+++ Tests/Octavo/test_bench/test_bench.cpp
@@ -8,7 +8,8 @@
 
 // Specific to the DUT and Verilator
 #include "VDUT.h"
-#include "verilated.h"
+#include <verilated.h>
+#include <verilated_vcd_c.h>
 
 int
 main
@@ -20,11 +21,24 @@ main
     int cycles_executed = 0;
     int cycles_to_run   = 1000; 
 
-	// Initialize Verilator's variables
+    // Current simulation time (64-bit unsigned)
+    vluint64_t main_time = 0;
+	
+    // Pass arguments so Verilated code can see them, e.g. $value$plusargs
 	Verilated::commandArgs(argc, argv);
+
+    // Set debug level, 0 is off, 9 is highest presently used
+    Verilated::debug(9);
 
 	// Create an instance of our Device Under Test
 	VDUT *DUT = new VDUT;
+
+    // Setup VCD dump
+    Verilated::traceEverOn(true);
+    VerilatedVcdC* vcd = new VerilatedVcdC;
+    // Trace 99 levels of hierarchy
+    DUT->trace(vcd, 99);
+    vcd->open("dump.vcd");
 
     // Make sure all state is initialized
 	DUT->clock = 0;
@@ -34,14 +48,25 @@ main
 	while(true) {
 		DUT->clock = 0;
 		DUT->eval();
+        vcd->dump(main_time);
+        main_time++;
+
 		DUT->clock = 1;
 		DUT->eval();
+        vcd->dump(main_time);
+        main_time++;
         
         cycles_executed++;
 
 	    if ( (cycles_executed == cycles_to_run) || (Verilated::gotFinish() == true) )
         {
             printf("Simulation terminated normally at cycle %u\n", cycles_executed);
+            DUT->final();
+            vcd->close();
+            delete vcd;
+            vcd = NULL;
+            delete DUT;
+            DUT = NULL;
             exit(EXIT_SUCCESS);
         }
 	}
