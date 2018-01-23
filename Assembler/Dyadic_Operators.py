cd53ae34b8a574d4f6ea300b8e9a065e785ebd61
Charles Eric LaForest <eric.laforest@gmail.com>
2018-01-23 15:48:44 -0500

    Move dyadic operators and ALU operators to bitstring format

--- Assembler/Dyadic_Operators.py
+++ Assembler/Dyadic_Operators.py
@@ -3,24 +3,27 @@
    Treat them as mux data inputs, LSB
    on the right, with a/b as the MSB/LSB selectors."""
 
+from bitstring import BitArray
+
 op_width     = 4
 
-always_zero  = 0b0000
-a_and_b      = 0b1000
-a_and_not_b  = 0b0100
-a            = 0b1100
-not_a_and_b  = 0b0010
-b            = 0b1010
-a_xor_b      = 0b0110
-a_or_b       = 0b1110
-a_nor_b      = 0b0001
-a_xnor_b     = 0b1001
-not_b        = 0b0101
-a_or_not_b   = 0b1101
-not_a        = 0b0011
-not_a_or_b   = 0b1011
-a_nand_b     = 0b0111
-always_one   = 0b1111
+always_zero  = BitArray("0b0000")
+a_and_b      = BitArray("0b1000")
+a_and_not_b  = BitArray("0b0100")
+a            = BitArray("0b1100")
+not_a_and_b  = BitArray("0b0010")
+b            = BitArray("0b1010")
+a_xor_b      = BitArray("0b0110")
+a_or_b       = BitArray("0b1110")
+a_nor_b      = BitArray("0b0001")
+a_xnor_b     = BitArray("0b1001")
+not_b        = BitArray("0b0101")
+a_or_not_b   = BitArray("0b1101")
+not_a        = BitArray("0b0011")
+not_a_or_b   = BitArray("0b1011")
+a_nand_b     = BitArray("0b0111")
+always_one   = BitArray("0b1111")
 
-
+if __name__ == "__main__":
+    print(a_or_not_b.bin)
 
--- Assembler/Triadic_ALU_Operators.py
+++ Assembler/Triadic_ALU_Operators.py
@@ -1,27 +1,28 @@
 
 """Control bit fields for the Triadic ALU"""
 
-import Dyadic_Operations as Dyadic
+from bitstring import BitArray
+import Dyadic_Operators as Dyadic
 
 # From Verilog code
 total_op_width          = 20
 
-select_r                = 0b00
-select_r_zero           = 0b01
-select_r_neg            = 0b10
-select_s                = 0b11
-simple                  = 0b0
-dual                    = 0b1
-addsub_a_plus_b         = 0b00
-addsub_minus_a_plus_b   = 0b01
-addsub_a_minus_b        = 0b10
-addsub_minus_a_minus_b  = 0b11
-shift_none              = 0b00
-shift_right             = 0b01
-shift_right_signed      = 0b10
-shift_left              = 0b11
-split_no                = 0b0
-split_yes               = 0b1
+select_r                = BitArray("0b00")
+select_r_zero           = BitArray("0b01")
+select_r_neg            = BitArray("0b10")
+select_s                = BitArray("0b11")
+simple                  = BitArray("0b0")
+dual                    = BitArray("0b1")
+addsub_a_plus_b         = BitArray("0b00")
+addsub_minus_a_plus_b   = BitArray("0b01")
+addsub_a_minus_b        = BitArray("0b10")
+addsub_minus_a_minus_b  = BitArray("0b11")
+shift_none              = BitArray("0b00")
+shift_right             = BitArray("0b01")
+shift_right_signed      = BitArray("0b10")
+shift_left              = BitArray("0b11")
+split_no                = BitArray("0b0")
+split_yes               = BitArray("0b1")
 
 select_width            = 2
 dyadic1_width           = Dyadic.op_width
@@ -33,4 +34,7 @@ shift_width             = 2
 split_width             = 1
 
 assert (select_width + dyadic1_width + dyadic2_width + dual_width + addsub_width + dyadic3_width + shift_width + split_width) == total_op_width, "ERROR: ALU control word width and sum of control bits widths do not agree"
+
+if __name__ == "__main__":
+    print(shift_right_signed.bin)
 
