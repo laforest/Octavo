8eaf5e9ac5e4c143395159172ec1bd5d55b20cb5
Charles Eric LaForest <eric.laforest@gmail.com>
2018-01-23 15:52:11 -0500

    Bring Opcode Decoder Memory to bitstring (so much simpler!)
    Oddly, there is no way to concatenate BitArrays? So append().

--- Assembler/Opcode_Decoder_Memory.py
+++ Assembler/Opcode_Decoder_Memory.py
@@ -2,6 +2,7 @@
 
 from Memory import Memory
 import Triadic_ALU_Operators as ALU
+from bitstring import pack,BitArray
 
 class Opcode_Decoder_Memory(Memory):
     """Extends Memory to assemble the control bits of each opcode."""
@@ -10,35 +11,24 @@ class Opcode_Decoder_Memory(Memory):
         assert width >= ALU.total_op_width, "ERROR: memory too narrow ({0} bits) to hold ALU control bits in {1}".format(width, self.__class__.__name__)
         Memory.__init__(self, file_name, depth = depth, width = width, write_offset = write_offset)
 
-        self.select_shift   = 0
-        self.dyadic1_shift  = ALU.select_width
-        self.dyadic2_shift  = self.dyadic1_shift + ALU.dyadic1_width
-        self.dual_shift     = self.dyadic2_shift + ALU.dyadic2_width
-        self.addsub_shift   = self.dual_shift    + ALU.dual_width
-        self.dyadic3_shift  = self.addsub_shift  + ALU.addsub_width
-        self.shift_shift    = self.dyadic3_shift + ALU.dyadic3_width 
-        self.split_shift    = self.shift_shift   + ALU.shift_width
+        self.alu_control_format = 'uint:{0},uint:{1},uint:{2},uint:{3},uint:{4},uint:{5},uint:{6},uint:{7}'.format(ALU.split_width, ALU.shift_width, ALU.dyadic3_width, ALU.addsub_width, ALU.dual_width, ALU.dyadic2_width, ALU.dyadic1_width, ALU.select_width)
 
-        self.select_mask    = self.width_mask(ALU.select_width)
-        self.dyadic1_mask   = self.width_mask(ALU.dyadic1_width)
-        self.dyadic2_mask   = self.width_mask(ALU.dyadic2_width)
-        self.dual_mask      = self.width_mask(ALU.dual_width)
-        self.addsub_mask    = self.width_mask(ALU.addsub_width)
-        self.dyadic3_mask   = self.width_mask(ALU.dyadic3_width)
-        self.shift_mask     = self.width_mask(ALU.shift_width)
-        self.split_mask     = self.width_mask(ALU.split_width)
-
-    def opcode(self, name, split, shift, dyadic3, addsub, dual, dyadic2, dyadic1, select):
-        """Assembles the control bits of an opcode. Names the opcode."""
-        control_bits  = (split   & self.split_mask)   << split_shift 
-        control_bits |= (shift   & self.shift_mask)   << shift_shift
-        control_bits |= (dyadic3 & self.dyadic3_mask) << dyadic3_shift
-        control_bits |= (addsub  & self.addsub_mask)  << addsub_shift
-        control_bits |= (dual    & self.dual_mask)    << dual_shift
-        control_bits |= (dyadic2 & self.dyadic2_mask) << dyadic2_shift
-        control_bits |= (dyadic1 & self.dyadic1_mask) << dyadic1_shift
-        control_bits |= (select  & self.select_mask)  << select_shift
-        self.lit(control_bits)
+    def opcode(self, name, opcode, split, shift, dyadic3, addsub, dual, dyadic2, dyadic1, select):
+        """Assembles the control bits of an opcode. Names the opcode. Opcode indexes memory."""
+        control_bits = BitArray()
+        for entry in [split, shift, dyadic3, addsub, dual, dyadic2, dyadic1, select]:
+            control_bits.append(entry)
+        self.align(opcode)
+        self.lit(control_bits.uint)
         self.loc(name)
 
+if __name__ == "__main__":
+    import Dyadic_Operators as Dyadic
+    OP_mem = Opcode_Decoder_Memory("foobar.mem", depth = 128, width = 20, write_offset = 1000)
+    op = 8
+    test = OP_mem.opcode("foo", op, ALU.split_yes, ALU.shift_left, Dyadic.not_a_or_b, ALU.addsub_a_minus_b, ALU.dual, Dyadic.a_xor_b, Dyadic.a_and_b, ALU.select_r_neg)
+    print(OP_mem.lookup("foo").length)
+    print(OP_mem.read_addr("foo"))
+    print(OP_mem.lookup("foo").bin)
+    print(OP_mem.lookup("foo").unpack(OP_mem.alu_control_format))
 
