5950b2d995d22bcd887ad194650a56dc89183350
Charles Eric LaForest <eric.laforest@gmail.com>
2018-03-03 20:35:02 -0500

    Comment out I/O Ready test-bench code. We know it works.

--- Tests/Octavo/test_bench/test_bench.cpp
+++ Tests/Octavo/test_bench/test_bench.cpp
@@ -58,15 +58,15 @@ main
         
         cycles_executed++;
 
-        // Test I/O write handshake, just on the zeroth port on Memory A
-        if ((cycles_executed > 100) && (cycles_executed < 300))
-        {
-            DUT->io_write_EF_A = 1;
-        } 
-        else
-        {
-            DUT->io_write_EF_A = 0;
-        }
+//        // Test I/O write handshake, just on the zeroth port on Memory A
+//        if ((cycles_executed > 100) && (cycles_executed < 200))
+//        {
+//            DUT->io_write_EF_A = 1;
+//        } 
+//        else
+//        {
+//            DUT->io_write_EF_A = 0;
+//        }
         
 
 	    if ( (cycles_executed == cycles_to_run) || (Verilated::gotFinish() == true) )
