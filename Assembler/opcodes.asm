fa08381bd9960135b86e0b2da60280966f01ccbe
Charles Eric LaForest <eric.laforest@gmail.com>
2018-09-10 20:14:57 -0400

    Note: ADD instruction *must* be present, else code generation gets hard...
    This way, we have a constant, known way to load data into config registers
    using the "Add To Zero" idiom.

--- Assembler/opcodes.asm
+++ Assembler/opcodes.asm
@@ -2,8 +2,10 @@
 # Define opcodes
 #       name    split?      shift               d3              +/-                 dual?   d2              d1              Select
 
-opcode  nop     split_no    shift_none          always_zero     addsub_a_plus_b     simple  always_zero     always_zero     select_r
+# Add must always be present for branch/indirect configuration loading code generation 
 opcode  add     split_no    shift_none          b               addsub_a_plus_b     simple  always_zero     always_zero     select_r
+
+opcode  nop     split_no    shift_none          always_zero     addsub_a_plus_b     simple  always_zero     always_zero     select_r
 opcode  sub     split_no    shift_none          b               addsub_a_minus_b    simple  always_zero     always_zero     select_r
 opcode  psr     split_no    shift_none          a               addsub_a_plus_b     simple  always_one      always_zero     select_r  # Pass R
 opcode  add*2   split_no    shift_left          b               addsub_a_minus_b    simple  always_zero     always_zero     select_r
