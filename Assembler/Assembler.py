a70654f85c9954f5044d603ab16d91ea4e09879f
Charles Eric LaForest <eric.laforest@gmail.com>
2018-10-02 21:05:46 -0400

    Data needs Configuration to assign port numbers and pointer slots.

--- Assembler/Assembler.py
+++ Assembler/Assembler.py
@@ -10,15 +10,15 @@ from Configuration  import Configuration
 from sys import argv
 
 if __name__ == "__main__":
-    data        = Data()
-    code        = Code(data)
-    commands    = Commands(data, code)
-    parser      = Parser(commands)
+    configuration   = Configuration()
+    data            = Data(configuration)
+    code            = Code(data)
+    commands        = Commands(data, code)
+    parser          = Parser(commands)
     parser.parse_file(argv[1])
     del parser
     del commands
     print("Parsing and Allocation Done")
-    configuration   = Configuration()
     resolver        = Resolver(data, code, configuration)
     resolver.resolve()
     print("Resolution Done")
