c85fa22a3c657ac9f853aa88c31ca3af285ec14c
Charles Eric LaForest <eric.laforest@gmail.com>
2019-01-11 17:13:51 -0500

    Add helper `defines for counting cycles.

--- Parts/SimSynth/simulation_clock.v
+++ Parts/SimSynth/simulation_clock.v
@@ -1,6 +1,13 @@
 
 // Clock generator for simulation
 
+// The following are handy to use with the resulting clock:
+
+// `define WAIT_CYCLES(n) repeat (n) begin @(posedge clock); end
+
+// time cycle = 0; 
+// `define UNTIL_CYCLE(n) wait (cycle == n);
+
 `default_nettype none
 
 `timescale 1 ns / 1 ps
